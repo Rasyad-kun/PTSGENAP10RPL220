02eb1ce2c37a71fefad623fb3d058f6832860dd3
「Г ѕ ч ᴅ」 <akhtarrasyadfahmi@gmail.com>
2021-04-04 21:30:30 +0700

    version 1.1.0

---
+++ app/src/main/java/com/example/ptsgenap10rpl220/part.java
@@ -0,0 +1,36 @@
+package com.example.ptsgenap10rpl220;
+
+public class part {
+    private String title, deskripsi;
+    private int icon;
+
+    public part(String title, String deskripsi, int icon) {
+        setTitle(title);
+        setDeskripsi(deskripsi);
+        setIcon(icon);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+}
